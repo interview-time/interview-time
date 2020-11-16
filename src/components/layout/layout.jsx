df761dc1ae7a82fa1ba4f19f7722bc62a46f969a
Peter Kovalsky <peter@192-168-1-6.tpgi.com.au>
2020-11-17 08:34:21 +1100

    initial commit

---
+++ src/components/layout/layout.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Layout as AntLayout, Menu } from "antd";
+import { Link, withRouter } from "react-router-dom";
+import {
+    MenuUnfoldOutlined,
+    MenuFoldOutlined,
+    UserOutlined,
+    VideoCameraOutlined,
+    UploadOutlined,
+} from '@ant-design/icons';
+
+const Layout = ({ children }) => {
+    const [collapsed, setCollapsed] = React.useState(false);
+
+    return (
+        <AntLayout id="components-layout-demo-custom-trigger" style={{
+            height: "100vh"
+        }}>
+            <AntLayout.Sider trigger={null} collapsible collapsed={collapsed}>
+                <div className="logo" />
+                <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                    <Menu.Item key="1" icon={<UserOutlined />}>
+                        nav 1
+              </Menu.Item>
+                    <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+                        nav 2
+              </Menu.Item>
+                    <Menu.Item key="3" icon={<UploadOutlined />}>
+                        nav 3
+              </Menu.Item>
+                </Menu>
+            </AntLayout.Sider>
+            <AntLayout className="site-layout">
+                <AntLayout.Header className="site-layout-background" style={{ padding: 0 }}>
+                    {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                        className: 'trigger',
+                        onClick: () => setCollapsed(!collapsed),
+                    })}
+                </AntLayout.Header>
+                <AntLayout.Content
+                    className="site-layout-background"
+                    style={{
+                        margin: '24px 16px',
+                        padding: 24,
+                        minHeight: 280,
+                    }}
+                >
+                    {children}
+                </AntLayout.Content>
+            </AntLayout>
+        </AntLayout>
+    )
+};
+
+export default Layout;
---
+++ src/pages/default/default.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../../components/layout/layout";
+
+const Default = () => {
+    return (
+        <Layout>
+            <h2>Interview Kit</h2>
+        </Layout>
+    )
+}
+
+export default Default;
