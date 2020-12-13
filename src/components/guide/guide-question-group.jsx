4169b8b4f100b8d01f7c5c9b17530e76004e9009
Dmytro Danylyk <dmytrodanylyk@gmail.com>
2020-12-13 14:44:16 +1100

    Guides - Add question screen (no drag and drop support)

---
+++ src/components/guide/guide-question-group.jsx
@@ -0,0 +1,146 @@
+import React, {useState} from 'react';
+import styles from "./guide-question-group.module.css";
+import {PlusCircleOutlined, DragOutlined, DeleteTwoTone} from '@ant-design/icons';
+import {List, Card, Space, Form, Button, Input, Row, Col, AutoComplete, Select, Tag} from "antd";
+import Typography from "antd/es/typography";
+
+const categories = [
+    {value: 'Android'},
+    {value: 'Java'},
+    {value: 'Behavioural'},
+];
+
+const {Search} = Input;
+const {Text} = Typography;
+
+const data = [
+    {
+        key: '1',
+        question: 'What are configuration changes and when do they happen?',
+        tags: ["android", "activity", "lifecycle"],
+        time: '2 min',
+    },
+    {
+        key: '2',
+        question: 'What is a broadcast receiver?',
+        tags: ["android", "activity", "lifecycle"],
+        time: '2 min',
+    },
+    {
+        key: '3',
+        question: 'Is it possible to create an activity in Android without a user interface ?',
+        tags: ["android", "activity", "lifecycle"],
+        time: '2 min',
+    },
+    {
+        key: '4',
+        question: 'Which method is called only once in a fragment life cycle?',
+        tags: ["android", "activity", "lifecycle"],
+        time: '2 min',
+    },
+    {
+        key: '5',
+        question: 'What is ANR, and why does it happen?',
+        tags: ["android", "activity", "lifecycle"],
+        time: '2 min',
+    },
+    {
+        key: '6',
+        question: 'How do you supply construction arguments into a Fragment?',
+        tags: ["android", "activity", "lifecycle"],
+        time: '2 min',
+    },
+    {
+        key: '7',
+        question: 'What is the difference between Service and IntentService? How is each used?',
+        tags: ["android", "activity", "lifecycle"],
+        time: '2 min',
+    },
+    {
+        key: '8',
+        question: 'What are “launch modes”? What are the two mechanisms by which they can be defined? What specific types of launch modes are supported?',
+        tags: ["android", "activity", "lifecycle"],
+        time: '2 min',
+    },
+];
+
+const GuideQuestionGroup = (props) => {
+
+    const [questions, setQuestions] = useState(data)
+
+    const onSearchTextChanged = e => {
+        onSearchClicked(e.target.value)
+    };
+
+    const onSearchClicked = (text) => {
+        let lowerCaseText = text.toLocaleLowerCase()
+        setQuestions(data.filter(item =>
+            item.question.toLocaleLowerCase().includes(lowerCaseText)
+            || item.tags.includes(lowerCaseText)
+        ))
+    }
+
+    return <Row>
+        <Col span={12}>
+            <Card className={styles.questionGroupCard} title="Question Group" bordered={false}>
+                <List
+                    dataSource={questions}
+                    renderItem={item => <List.Item>
+                        <div className={styles.questionBody}>
+                            <Space><DragOutlined className={styles.dragIcon} /></Space>
+                            <Space direction="vertical">
+                                <Text>{item.question}</Text>
+                                <div>
+                                    {item.tags.map(tag => <Tag key={tag}>{tag.toLowerCase()}</Tag>)}
+                                </div>
+                            </Space>
+                        </div>
+
+                        <Space direction="vertical" align="end" className={styles.metaBody}>
+                            <Text>{item.time}</Text>
+                            <DeleteTwoTone twoToneColor="red" />
+                        </Space>
+                    </List.Item>}
+                />
+            </Card>
+        </Col>
+        <Col span={12}>
+            <Card className={styles.questionBankCard} title="Question Bank" bordered={false}>
+                <div className={styles.questionBankBody}>
+                    <Select
+                        className={styles.categorySelect}
+                        options={categories}
+                        placeholder="Category"
+                        showSearch
+                        filterOption={(inputValue, option) =>
+                            option.value.toLocaleLowerCase().includes(inputValue)
+                        }
+                    />
+                    <Search placeholder="Search" allowClear enterButton onSearch={onSearchClicked}
+                            onChange={onSearchTextChanged} />
+                </div>
+
+                <List
+                    dataSource={questions}
+                    renderItem={item => <List.Item>
+                        <div className={styles.questionBody}>
+                            <Space><DragOutlined className={styles.dragIcon} /></Space>
+                            <Space direction="vertical">
+                                <Text>{item.question}</Text>
+                                <div>
+                                    {item.tags.map(tag => <Tag key={tag}>{tag.toLowerCase()}</Tag>)}
+                                </div>
+                            </Space>
+                        </div>
+
+                        <Space direction="vertical" align="end" className={styles.metaBody}>
+                            <Text>{item.time}</Text>
+                        </Space>
+                    </List.Item>}
+                />
+            </Card>
+        </Col>
+    </Row>
+}
+
+export default GuideQuestionGroup
--- src/pages/guide-details/guide-details.jsx
+++ src/pages/guide-details/guide-details.jsx
@@ -1,13 +1,12 @@
 import React, {useState} from "react";
 import Layout from "../../components/layout/layout";
 import styles from "./guide-details.module.css";
-import {Typography, Select, Form, Input, Button, PageHeader, Row, Col, Card, Tabs} from 'antd';
+import {Select, Form, Input, Button, PageHeader, Row, Col, Card, Tabs} from 'antd';
 import {Link} from "react-router-dom";
 import GuideStructureCard from "../../components/guide/guide-structure-card";
+import GuideQuestionGroup from "../../components/guide/guide-question-group";
 import InterviewDetailsCard from "../../components/interview/interview-details-card";
 import Text from "antd/es/typography/Text";
-
-const {Paragraph} = Typography;
 
 const {TabPane} = Tabs;
 const {TextArea} = Input;
@@ -28,8 +27,9 @@ const tailLayout = {
 };
 
 const STEP_DETAILS = 1
-const STEP_EDIT = 2
-const STEP_PREVIEW = 3
+const STEP_STRUCTURE = 2
+const STEP_QUESTIONS = 3
+const STEP_PREVIEW = 4
 
 const TAB_DETAILS = "details"
 const TAB_STRUCTURE = "structure"
@@ -41,7 +41,9 @@ const GuideDetails = () => {
 
     const isPreviewStep = () => step === STEP_PREVIEW
 
-    const isEditStep = () => step === STEP_EDIT
+    const isStructureStep = () => step === STEP_STRUCTURE
+
+    const isQuestionsStep = () => step === STEP_QUESTIONS
 
     const onBackClicked = () => {
         window.history.back()
@@ -51,23 +53,23 @@ const GuideDetails = () => {
         if (key === TAB_DETAILS) {
             setStep(STEP_DETAILS)
         } else if (key === TAB_STRUCTURE) {
-            setStep(STEP_EDIT)
+            setStep(STEP_STRUCTURE)
         }
     }
 
     const getHeaderTitle = () => {
-        if (isDetailsStep() || isEditStep()) {
-            // TODO replace with "Edit Interview Guide" based on state
-            return "New Interview Guide"
-        } else if (isPreviewStep()) {
+        if (isPreviewStep()) {
             return "Interviewer Experience"
+        } else {
+            // TODO replace with "Edit Interview Guide" if we are editing existing guide
+            return "New Interview Guide"
         }
     }
 
     const getActiveTab = () => {
         if (isDetailsStep()) {
             return TAB_DETAILS
-        } else if (isEditStep()) {
+        } else if (isStructureStep()) {
             return TAB_STRUCTURE
         }
     }
@@ -77,20 +79,29 @@ const GuideDetails = () => {
         onBack={() => onBackClicked()}
         title={getHeaderTitle()}
         extra={[
-            <>{(isPreviewStep()) && <Button type="default" onClick={() => setStep(STEP_EDIT)}>
+            <>{(isPreviewStep()) && <Button type="default" onClick={() => setStep(STEP_STRUCTURE)}>
                 <span className="nav-text">Edit</span>
             </Button>}</>,
-            <>{(isDetailsStep() || isEditStep()) && <Button type="default" onClick={() => setStep(STEP_PREVIEW)}>
+            <>{(isDetailsStep() || isStructureStep()) &&
+            <Button type="default" onClick={() => setStep(STEP_PREVIEW)}>
                 <span className="nav-text">Preview</span>
             </Button>}</>,
-            <Button type="primary">
+            <>{isQuestionsStep() &&
+            <Button type="default" onClick={() => setStep(STEP_STRUCTURE)}>
+                <span className="nav-text">Discard</span>
+            </Button>}</>,
+            <>{isQuestionsStep() && <Button type="primary" onClick={() => setStep(STEP_STRUCTURE)}>
+                <span className="nav-text">Done</span>
+            </Button>}</>,
+            <>{(!isQuestionsStep()) && <Button type="primary">
                 <Link to={`/guides`}>
                     <span className="nav-text">Save</span>
                 </Link>
-            </Button>
+            </Button>}</>
         ]}
         footer={
-            <>{(isDetailsStep() || isEditStep()) && <Tabs defaultActiveKey={getActiveTab} onChange={onTabClicked}>
+            <>{(isDetailsStep() || isStructureStep()) &&
+            <Tabs defaultActiveKey={getActiveTab} onChange={onTabClicked}>
                 <TabPane tab="Details" key={TAB_DETAILS} />
                 <TabPane tab="Structure" key={TAB_STRUCTURE} />
             </Tabs>}</>
@@ -99,15 +110,21 @@ const GuideDetails = () => {
         {isDetailsStep() && <Text>
             Enter interview guide detail information so you can easily discover it among other guides.</Text>}
         {isPreviewStep() && <Text>
-            This is a <Text strong>preview</Text> of the guide which will be used during the interview. To go back click on <Text
-            strong>edit</Text> button.</Text>}
-        {isEditStep() && <Text>
+            This is a <Text strong>preview</Text> of the guide which will be used during the interview. To go back click
+            on <Text strong>edit</Text> button.</Text>}
+        {isStructureStep() && <Text>
             Stay organized and structure the interview guide. <Text strong>Grouping</Text> questions helps to evaluate
-            skills in a
-            particular area and make a more granular assessment. Click on the <Text strong>preview</Text> button to see
+            skills in a particular area and make a more granular assessment. Click on the <Text
+            strong>preview</Text> button to see
             the changes.</Text>}
+        {isQuestionsStep() && <Text>
+            Drag and drop questions from your question bank to the question group.
+            </Text>}
     </PageHeader>}>
         <Row gutter={16} justify="center">
+            {isQuestionsStep() && <Col span={24}>
+                <GuideQuestionGroup />
+            </Col>}
             {isDetailsStep() && <Col className={styles.detailsCard}>
                 <Card title="Guide Details" bordered={false} headStyle={{textAlign: 'center'}}>
                     <Form
@@ -146,10 +163,11 @@ const GuideDetails = () => {
             </Col>}
             {isPreviewStep() && <Col span={24}>
                 <InterviewDetailsCard />
-            </Col>
-            }
-            {isEditStep() && <Col>
-                <GuideStructureCard showRevertButton={false} />
+            </Col>}
+            {isStructureStep() && <Col>
+                <GuideStructureCard
+                    showRevertButton={false}
+                    onAddQuestionClicked={() => setStep(STEP_QUESTIONS)} />
             </Col>}
         </Row>
     </Layout>
