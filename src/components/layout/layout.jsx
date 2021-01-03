6bb582f66becbe6d6e0673801e6c4af3fce15ae5
Dmytro Danylyk <dmytrodanylyk@gmail.com>
2021-01-03 13:20:00 +1100

    1. Updated favicon and logos 2. Updated overall layout and icons

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,67 +1,46 @@
 import React from "react";
-import {Layout as AntLayout, Menu} from "antd";
-import {Link, withRouter} from "react-router-dom";
-import {
-    MenuUnfoldOutlined,
-    MenuFoldOutlined,
-    DashboardOutlined,
-    QuestionOutlined,
-    FormOutlined,
-    CheckCircleOutlined,
-    UserOutlined,
-    QuestionCircleOutlined,
-} from '@ant-design/icons';
+import {Divider, Layout as AntLayout, Menu, Space} from "antd";
+import {Link} from "react-router-dom";
+import Avatar from "antd/es/avatar/avatar";
+import Text from "antd/es/typography/Text";
+import styles from "./layout.module.css";
+import {QuestionBankIcon, GuideIcon, InterviewIcon} from "../utils/icons";
+
+const menuIconStyle = { fontSize: '24px' }
 
 const Layout = ({children, pageHeader}) => {
-    const [collapsed, setCollapsed] = React.useState(false);
-
     return (
-        <AntLayout id="components-layout-demo-custom-trigger" style={{
-            height: "100vh"
-        }}>
-            <AntLayout.Sider trigger={null} collapsible collapsed={collapsed}>
-                <div className="logo" />
-                <Menu theme="dark" mode="inline" defaultSelectedKeys={['question-bank']}>
-                    <Menu.Item key="1" icon={<DashboardOutlined />}>
-                        Dashboard
-                    </Menu.Item>
-                    <Menu.Item key="question-bank" icon={<QuestionOutlined />}>
+        <AntLayout className={styles.globalLayout}>
+            <AntLayout.Sider theme='light' className={styles.globalSider}>
+                <img src={process.env.PUBLIC_URL + '/logo+text.png'} className={styles.logo} />
+                <Menu theme="light" mode="inline" defaultSelectedKeys={['question-bank']} className={styles.menu}>
+                    <Menu.Item key="question-bank" className={styles.menuItem} icon={<QuestionBankIcon style={menuIconStyle}/>}>
                         <Link to={`/question-bank`}>
                             <span className="nav-text">Question Bank</span>
                         </Link>
                     </Menu.Item>
-                    <Menu.Item key="guides" icon={<FormOutlined />}>
+                    <Menu.Item key="guides" className={styles.menuItem} icon={<GuideIcon style={menuIconStyle}/>}>
                         <Link to={`/guides`}>
                             <span className="nav-text">Guides</span>
                         </Link>
                     </Menu.Item>
-                    <Menu.Item key="interviews" icon={<CheckCircleOutlined />}>
+                    <Menu.Item key="interviews" className={styles.menuItem} icon={<InterviewIcon style={menuIconStyle}/>}>
                         <Link to={`/interviews`}>
                             <span className="nav-text">Interviews</span>
                         </Link>
                     </Menu.Item>
-                    <Menu.Item key="4" icon={<UserOutlined />}>
-                        Account
-                    </Menu.Item>
-                    <Menu.Item key="5" icon={<QuestionCircleOutlined />}>
-                        Help
-                    </Menu.Item>
                 </Menu>
+                <Divider />
+                <Space className={styles.avatar}>
+                    <Avatar
+                        src='https://cdn.dribbble.com/users/3844750/screenshots/10729124/media/2523facfa3e436b8331c316dcc4998f2.jpg'
+                        size="32" />
+                    <Text>Kristin Watson</Text>
+                </Space>
             </AntLayout.Sider>
             <AntLayout className="site-layout">
-                <AntLayout.Header className="site-layout-background" style={{padding: 0}}>
-                    {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-                        className: 'trigger',
-                        onClick: () => setCollapsed(!collapsed),
-                    })}
-                </AntLayout.Header>
                 {pageHeader}
-                <AntLayout.Content
-                    style={{
-                        padding: 24,
-                        minHeight: 280,
-                    }}
-                >
+                <AntLayout.Content className={styles.pageContent}>
                     {children}
                 </AntLayout.Content>
             </AntLayout>
