8fe8957cbd3b4fbb4c266abec358a3c38156b22f
Peter <peter.kovalskyy@gmail.com>
2021-01-09 11:36:05 +1100

    removed duplicate prop

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -22,7 +22,7 @@ const Layout = ({children, pageHeader}) => {
     return (
         <AntLayout className={styles.globalLayout}>
             <AntLayout.Sider theme='light' className={styles.globalSider}>
-                <img alt="Interviwer" src={process.env.PUBLIC_URL + '/logo+text.png'} className={styles.logo} alt='Avatar' />
+                <img alt="Interviwer" src={process.env.PUBLIC_URL + '/logo+text.png'} className={styles.logo} />
                 <Menu theme="light"
                       mode="inline"
                       defaultSelectedKeys={[PATH_QUESTION_BANK]}
