f0f3767d317193ddf41c217fb3eea71d40d2b0da
Dmytro Danylyk <dmytrodanylyk@gmail.com>
2022-01-12 20:01:17 +1000

    Team switch bug fixes: - fixed bug when user was seeing team id when switched user profile - fixed bug when user was seeing old team name when joined new team

--- src/store/common.js
+++ src/store/common.js
@@ -16,7 +16,3 @@ export const getActiveTeamId = () => {
     const team = getCachedActiveTeam();
     return team ? team.teamId : undefined;
 }
-
-export const STATUS_STARTED = "STATUS_STARTED"
-export const STATUS_FINISHED = "STATUS_FINISHED"
-export const STATUS_ERROR = "STATUS_ERROR"
